e3b8a52b6a43f2ebe58b43d7d7cb8cf80ddd4a88
isaaccheaz06 <idc2033@nyu.edu>
2025-05-11 13:25:18 +0800

    fixed a lot of stuff

--- project-b-protype/sketch.js
+++ project-b-protype/sketch.js
@@ -1,28 +1,33 @@
-let faceMesh;
 let handPose;
 let video;
 let hands = [];
-let faces = [];
-let options = { maxFaces: 2, refineLandmarks: false, flipHorizontal: false };
-let options2 = { maxHands: 4, modelType: "lite" };
+let options = { maxHands: 2 };
 
-let vidWidth = 640;
-let vidHeight = 480;
+let startTime;
+let showIntro = true;
+let showVideo = false;
+let NowStartDrawing = false;
+
+let vidWidth = 1056;
+let vidHeight = 792;
+
+let pinch = 0;
+
+let drawingPoints = [];
 
 function preload() {
-  faceMesh = ml5.faceMesh(options);
-  handPose = ml5.handPose(options2);
+  handPose = ml5.handPose(options);
 }
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("p5-canvas-container");
+  createCanvas(windowWidth, windowHeight);
 
   video = createCapture(VIDEO);
   video.size(vidWidth, vidHeight);
   video.hide();
 
-  faceMesh.detectStart(video, gotFaces);
+  startTime = millis();
+
   handPose.detectStart(video, gotHands);
   background(255);
 }
@@ -30,112 +35,136 @@ function setup() {
 function draw() {
   background(255);
 
-  let xOffset = (width - vidWidth) / 2;
-  let yOffset = (height - vidHeight) / 2;
-  push();
-  translate(xOffset + vidWidth, yOffset);
-  scale(-1, 1);
-  image(video, 1000, 1000, vidWidth, vidHeight);
-  pop();
+  let elapsed = millis() - startTime;
 
-  let gridSize = 15;
+  if (showIntro) {
+    let alpha;
+
+    if (elapsed < 2000) {
+      alpha = map(elapsed, 0, 2000, 0, 255);
+    } else if (elapsed < 7000) {
+      alpha = 255;
+    } else if (elapsed < 10000) {
+      alpha = map(elapsed, 7000, 10000, 255, 0);
+    } else if (elapsed >= 10000) {
+      showIntro = false;
+      showVideo = true;
+      return;
+    }
+
+    fill(0, alpha);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    textSize(64);
+    text("YOU ARE ART", width / 2, height / 2);
+  }
+
+  let yOffset = (height - vidHeight) / 3;
+  let gridSize = 20;
+
   video.loadPixels();
 
-  push();
-  translate(xOffset + vidWidth, yOffset);
-  scale(-1, 1);
+  if (showVideo) {
+    let videoalpha = 0;
 
-  // Grid distortion
-  for (let y = 0; y < video.height; y += gridSize) {
-    stroke(0);
-    noFill();
-    beginShape();
-    for (let x = 0; x < video.width; x += gridSize) {
-      let index = (x + y * video.width) * 4;
-
-      let r = video.pixels[index + 0];
-      let g = video.pixels[index + 1];
-      let b = video.pixels[index + 2];
-
-      let avg = (r + g + b) / 3;
-      let yAdj = map(avg, 0, 255, 0, gridSize * 2);
-
-      curveVertex(x, y + yAdj);
+    if (elapsed < 15000) {
+      videoalpha = map(elapsed, 10000, 15000, 0, 255);
+    } else {
+      videoalpha = 255;
+      NowStartDrawing = true;
     }
-    endShape();
-  }
 
-  // Face keypoints (colored)
-  for (let i = 0; i < faces.length; i++) {
-    let face = faces[i];
-    for (let j = 0; j < face.keypoints.length; j++) {
-      let keypoint = face.keypoints[j];
+    push();
+    translate(width / 2, yOffset);
+    scale(-1, 1);
 
-      let px = floor(keypoint.x);
-      let py = floor(keypoint.y);
+    // Grid distortion
+    for (let y = 0; y < video.height; y += gridSize) {
+      stroke(0, videoalpha);
+      noFill();
+      beginShape();
+      for (let x = 0; x < video.width; x += gridSize) {
+        let index = (x + y * video.width) * 4;
 
-      if (px >= 0 && px < video.width && py >= 0 && py < video.height) {
-        let index = (px + py * video.width) * 4;
         let r = video.pixels[index + 0];
         let g = video.pixels[index + 1];
         let b = video.pixels[index + 2];
 
-        fill(r, g, b);
-        noStroke();
-        circle(keypoint.x, keypoint.y, 5);
+        let avg = (r + g + b) / 3;
+        let yAdj = map(avg, 0, 255, 0, gridSize * 2);
+
+        curveVertex(x - vidWidth / 2, y + yAdj);
+      }
+      endShape();
+    }
+
+    // Hand keypoints and connections
+    const connections = [
+      [0, 1], [1, 2], [2, 3], [3, 4],
+      [5, 6], [6, 7], [7, 8],
+      [9, 10], [10, 11], [11, 12],
+      [13, 14], [14, 15], [15, 16],
+      [0, 17], [17, 18], [18, 19], [19, 20],
+      [1, 5], [5, 9], [9, 13], [13, 17],
+    ];
+
+    for (let i = 0; i < hands.length; i++) {
+      let hand = hands[i];
+      let keypoints = hand.keypoints;
+
+      // Draw connecting lines
+      stroke(0);
+      strokeWeight(2);
+      for (let k = 0; k < connections.length; k++) {
+        let [i1, i2] = connections[k];
+        let kp1 = keypoints[i1];
+        let kp2 = keypoints[i2];
+        line(kp1.x - vidWidth / 2, kp1.y, kp2.x - vidWidth / 2, kp2.y);
       }
     }
+
+
   }
 
-  // Hand keypoints and connections
-  const connections = [
-    [0, 1], [1, 2], [2, 3], [3, 4],
-    [5, 6], [6, 7], [7, 8],
-    [9, 10], [10, 11], [11, 12],
-    [13, 14], [14, 15], [15, 16],
-    [0, 17], [17, 18], [18, 19], [19, 20],
-    [1, 5], [5, 9], [9, 13], [13, 17],
+  if (NowStartDrawing) {
+    for (let i = 0; i < hands.length; i++) {
+      let hand = hands[i];
+      let keypoints = hand.keypoints;
 
-  ];
+      let thumb = keypoints[4];
+      let index = keypoints[8];
+      let middle = keypoints[12];
 
-  for (let i = 0; i < hands.length; i++) {
-    let hand = hands[i];
-    let keypoints = hand.keypoints;
+      let centerX = (index.x + thumb.x) / 2;
+      let centerY = (index.y + thumb.y) / 2;
 
-    // Draw connecting lines
-    stroke(0);
-    strokeWeight(2);
-    for (let k = 0; k < connections.length; k++) {
-      let [i1, i2] = connections[k];
-      let kp1 = keypoints[i1];
-      let kp2 = keypoints[i2];
-      line(kp1.x, kp1.y, kp2.x, kp2.y);
+      let centerX2 = (middle.x + thumb.x) / 2;
+      let centerY2 = (middle.y + thumb.y) / 2;
+
+      let pinchDist = dist(index.x, index.y, thumb.x, thumb.y);
+      let pinchDist2 = dist(middle.x, middle.y, thumb.x, thumb.y);
+
+      if (pinchDist < 50) {
+        let randColor = color(random(255), random(255), random(255));
+        drawingPoints.push({ x: centerX, y: centerY });
+      }
+
+      if (pinchDist2 < 20) {
+        drawingPoints = [];
+      }
+
     }
 
-    // Draw keypoints using video pixel color
+    // Draw all stored points
     noStroke();
-    for (let j = 0; j < keypoints.length; j++) {
-      let kp = keypoints[j];
-      let px = floor(kp.x);
-      let py = floor(kp.y);
-
-      if (px >= 0 && px < video.width && py >= 0 && py < video.height) {
-        let index = (px + py * video.width) * 4;
-        let r = video.pixels[index + 0];
-        let g = video.pixels[index + 1];
-        let b = video.pixels[index + 2];
-
-        fill(r, g, b);
-        circle(kp.x, kp.y, 8);
-      }
+    fill(0);
+    for (let pt of drawingPoints) {
+      circle(pt.x - vidWidth / 2, pt.y, 20);
     }
   }
 
-  pop(); // end mirror transform
-}
 
-function gotFaces(results) {
-  faces = results;
+  pop();
 }
 
 function gotHands(results) {
